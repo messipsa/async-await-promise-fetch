3bd3ec04167deedffe6a76e9cb4de8dfaa5ab173
Messipsa <im_tamazouzt@esi.dz>
2020-12-07 21:05:07 +0100

    initial

---
+++ task1.js
@@ -0,0 +1,28 @@
+// Task 1: Translate the story into code.
+const mybirthday = (Sick) => {
+     return new Promise ((resolve,reject)=>{
+         setTimeout(()=>{
+            if(!Sick)
+            {
+               resolve(2);
+            }
+            else
+            {
+                 reject(new Error ("I am sad"));
+            }
+         },2000);
+         
+     }
+
+     );
+}
+console.time("Timer" );
+
+mybirthday(true)
+.then((result)=>{
+    console.timeLog("Timer");
+    console.log(`We prepared ${result} cakes.`);
+}).catch((error)=>{
+    console.timeLog("Timer");
+    console.log(error);
+});
